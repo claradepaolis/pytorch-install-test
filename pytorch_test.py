7771c8ab5f0efc62ebf63acadc9f5f175351efaf
Clara De Paolis <claradepaolis@users.noreply.github.com>
2022-11-17 21:34:00 -0500

    test scripts

---
+++ pytorch_test.py
@@ -0,0 +1,39 @@
+import os
+import torch
+from torch import nn
+
+
+device = "cpu"
+print(f"Using {device} device")
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
+
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
+
+
+
---
+++ pytorch_test_tensorboard.py
@@ -0,0 +1,114 @@
+# adapted from: https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
+# This code should overfit to the test data since it is trained on it
+# This code is meant to test GPU functionality
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.tensorboard import SummaryWriter
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10),
+        )
+    @property
+    def device(self):
+        return next(self.parameters()).device 
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X.to(model.device))
+        loss = loss_fn(pred, y.to(model.device))
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if batch % 100 == 0:
+            loss, current = loss.cpu().item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    return loss.cpu().item()
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X.to(model.device)).cpu()
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loss, 100*correct
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+print('data loaders')
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+print(test_dataloader)
+
+device = "cuda:2" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+print('themodel')
+model = NeuralNetwork().to(device)
+print(model)
+print(model.device)
+
+learning_rate = 1e-2
+batch_size = 64
+
+#
+writer = SummaryWriter()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loss = train_loop(train_dataloader, model, loss_fn, optimizer)
+    writer.add_scalar("Loss/train", train_loss, t)
+    test_loss, test_acc = test_loop(test_dataloader, model, loss_fn)
+    writer.add_scalar("Loss/test", test_loss, t)
+    writer.add_scalar("Accuracy/test", test_acc, t)
+print("Done!")
+
